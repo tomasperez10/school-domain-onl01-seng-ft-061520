93441d4d5d51d583ff62fc4270ff2020c2b67d04
Tomas Perez <tomasitopg6@gmail.com>
2020-06-26 00:41:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -22,10 +22,10 @@ class School
   end
   
   def sort
-      nu_hash = {}
+      new_hash = {}
       roster.each do | x, y |
-        nu_hash[x] = y.sort
+        new_hash[x] = y.sort
       end
-      nu_hash
+      new_hash
     end
 end
